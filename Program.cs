d036cec9627d3673868a3f48f3f6358f624b26ed
Matthias Mertens <matthias0212@gmail.com>
2023-01-21 19:14:46 +0100

    Empty tool

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Spectre.Console;
+
+AnsiConsole.WriteLine("Hello, World!");
+var name = AnsiConsole.Ask<string>("What's your [green]name[/]?");
